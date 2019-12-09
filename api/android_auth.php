21d52ab1bb5681e31b31debf454860afcc499a89
Appanalytics <Sambhrant@appanalytics.in>
2019-12-09 20:02:42 +0530

    updated

--- api/android_auth.php
+++ api/android_auth.php
@@ -5,12 +5,13 @@ try{
 	{
 		if($_SERVER["REQUEST_METHOD"]=="GET"){
 			echo json_encode(array("status"=>"0","message"=>"Failed","reason"=>"Web browser requests not acceptable for this API."));
+			exit();
 		}
 		// ################ CUSTOMER LOGIN #####################
 		if($_REQUEST["Key"]=="android565656"){
-			if(isset($_POST["username"])&&isset($_POST["password"])){
-				$username = trim($_POST["username"]);
-				$password = trim($_POST["password"]);
+			if(isset($_REQUEST["username"])&&isset($_REQUEST["password"])){
+				$username = trim($_REQUEST["username"]);
+				$password = trim($_REQUEST["password"]);
 				$qry="SELECT `Name` from `cx_userinfo` WHERE (`Email`='$username' OR `Phone`='$username') AND `Password`='$password'";
 				$result=mysqli_query($conn, $qry);
 				if(mysql_num_rows($result)>0){
@@ -23,11 +24,11 @@ try{
 				else
 					$data=array("status"=>"0","message"=>"Failed","reason"=>"Username/Password does not match.");
 			}
-			else if(isset($_POST["name"])&&isset($_POST["email"])&&isset($_POST["phone"])&&isset($_POST["pwd"])){
-				$name = check_input($_POST['name']);
-				$email = check_input($_POST['email']);
-				$phone = check_input($_POST['phone']);
-				$hashed_password = password_hash(trim($_POST['pwd']), PASSWORD_BCRYPT);
+			else if(isset($_REQUEST["name"])&&isset($_REQUEST["email"])&&isset($_REQUEST["phone"])&&isset($_REQUEST["pwd"])){
+				$name = check_input($_REQUEST['name']);
+				$email = check_input($_REQUEST['email']);
+				$phone = check_input($_REQUEST['phone']);
+				$hashed_password = password_hash(trim($_REQUEST['pwd']), PASSWORD_BCRYPT);
 		        $sqlCommand = "INSERT INTO `cx_userinfo` VALUES(DEFAULT,'".mysqli_real_escape_string($conn, $name)."','".mysqli_real_escape_string($conn, $email)."','".mysqli_real_escape_string($conn, $phone)."','".mysqli_real_escape_string($conn, $hashed_password)."',DEFAULT,DEFAULT)";
 		        // echo $sqlCommand;
 		        $query=mysqli_query($conn,$sqlCommand);
@@ -40,9 +41,9 @@ try{
 		}
 		// ################ CLIENT LOGIN #####################
 		else if($_REQUEST["Key"]=="android565656:client"){
-			if(isset($_POST["username"])&&isset($_POST["password"])){
-				$username = trim($_POST["username"]);
-				$password = trim($_POST["password"]);
+			if(isset($_REQUEST["username"])&&isset($_REQUEST["password"])){
+				$username = trim($_REQUEST["username"]);
+				$password = trim($_REQUEST["password"]);
 				$qry="SELECT `Name` from `cx_userinfo` WHERE (`Email`='$username' OR `Phone`='$username') AND `Password`='$password'";
 				$result=mysqli_query($conn, $qry);
 				if(mysql_num_rows($result)>0){
@@ -56,12 +57,12 @@ try{
 					$data=array("status"=>"0","message"=>"Failed","reason"=>"Username/Password does not match.");
 
 			}
-			else if(isset($_POST["name"])&&isset($_POST["email"])&&isset($_POST["phone"])&&isset($_POST["pwd"])&&isset($_POST["restaurant_name"])){
-				$name = check_input($_POST['name']);
-				$email = check_input($_POST['email']);
-				$phone = check_input($_POST['phone']);
-				$$restaurantName = check_input($_POST['restaurant_name']);
-				$client_password = trim($_POST['pwd']);
+			else if(isset($_REQUEST["name"])&&isset($_REQUEST["email"])&&isset($_REQUEST["phone"])&&isset($_REQUEST["pwd"])&&isset($_REQUEST["restaurant_name"])){
+				$name = check_input($_REQUEST['name']);
+				$email = check_input($_REQUEST['email']);
+				$phone = check_input($_REQUEST['phone']);
+				$$restaurantName = check_input($_REQUEST['restaurant_name']);
+				$client_password = trim($_REQUEST['pwd']);
 
 				$sqlCommand = "SELECT * FROM client_userinfo WHERE Email='".mysqli_real_escape_string($con, $client_email)."'";
 				$query=mysqli_query($con,$sqlCommand);
@@ -72,7 +73,7 @@ try{
 				
 				if (!is_null($result))      
 					$data=array("status"=>"0","message"=>"Failed","reason"=>"Email already registered.");
-				if (!is_null($result2)){      
+				if (!is_null($result2))  
 					$data=array("status"=>"0","message"=>"Failed","reason"=>"Phone number already registered.");
 
 			    else if (is_null($result) && is_null($result2)){
@@ -95,8 +96,8 @@ try{
 			            "Address" => mysqli_real_escape_string($conn, str_replace("\n", " ",  $adrs)),
 			            "State" => mysqli_real_escape_string($conn, $stateName),
 			            "City" => mysqli_real_escape_string($conn, $cityName),
-			            "Pin Code" => mysqli_real_escape_string($conn, $_POST['PinCode']),
-			            "Designation" => mysqli_real_escape_string($conn, $_POST['Designation']),
+			            "Pin Code" => mysqli_real_escape_string($conn, $_REQUEST['PinCode']),
+			            "Designation" => mysqli_real_escape_string($conn, $_REQUEST['Designation']),
 			            "data" => true,
 			        );
 			        $json = json_creator($jsonArray);
